eaacd14bfc44400e0fa6e1fc389485e6ee647aae
The King wilker <sandovalwilkerson700@gmail.com>
2026-05-16 01:14:17 -0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+
+global.owner = ['526242255295','526241181534']
+global.botNumber = ''
+
+global.sessionName = 'Sessions/Owner'
+global.version = '^0.0.11 - Latest'
+global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ 𝔇ᥲᥒⁱᥱᥣᵣ᥊ᶻ"
+global.wm = "Miku-wabot"
+global.links = {
+api: 'senko api',
+channel: "https://whatsapp.com/channel/0029VbBUzJ6DzgT8o9NiMq2b",
+github: "https://github.com/Dani-land",
+gmail: "danielrodigome@gmail.com"
+}
+global.my = {
+ch: '120363420575743790@newsletter',
+name: '✦ 𝑴𝒊𝒌𝒖 𝑨𝑰 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 ﻿✦',
+}
+
+global.mess = {
+socket: '✐ Este comando solo puede ser ejecutado por un Socket.',
+admin: '✐ Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+botAdmin: '✐ Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
+}
+
+global.APIs = {
+adonix: { url: "https://api-adonix.ultraplus.click", key: "Danielrxz" },
+vreden: { url: "https://api.vreden.web.id", key: null },
+nekolabs: { url: "https://api.nekolabs.web.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+stellar: { url: "https://api.stellarwa.xyz", key: wm },
+apifaa: { url: "https://api-faa.my.id", key: null },
+xyro: { url: "https://api.xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null }
+}
