16dca385812a75241f850982b13ed4a8b3136e9f
The King wilker <sandovalwilkerson700@gmail.com>
2026-05-16 04:08:23 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -1,27 +1,27 @@
 import fs from 'fs';
 
-global.owner = ['526242255295','526241181534']
+global.owner = ['5492644138998','584125877491']
 global.botNumber = ''
 
 global.sessionName = 'Sessions/Owner'
-global.version = '^0.0.11 - Latest'
-global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ 𝔇ᥲᥒⁱᥱᥣᵣ᥊ᶻ"
-global.wm = "Miku-wabot"
+global.version = '🐉🌀 3.0 - Gotenks V1 🌀🐉'
+global.dev = "⚡ Powered by Gotenks V1 Bot ⚡"
+global.wm = "Gotenks-V1 🐉"
 global.links = {
-api: 'senko api',
-channel: "https://whatsapp.com/channel/0029VbBUzJ6DzgT8o9NiMq2b",
-github: "https://github.com/Dani-land",
-gmail: "danielrodigome@gmail.com"
+api: 'Gotenks API 🌀',
+channel: "https://whatsapp.com/channel/0029Vb7ntULLY6d8uOvyDy0C",
+github: "https://github.com/dvwilker",
+gmail: "developer.wilker.ofc@gmail.com"
 }
 global.my = {
-ch: '120363420575743790@newsletter',
-name: '✦ 𝑴𝒊𝒌𝒖 𝑨𝑰 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 ﻿✦',
+ch: '120363404707199986@newsletter',
+name: '🐉✦ Gotenks V1 Bot ✦🌀',
 }
 
 global.mess = {
-socket: '✐ Este comando solo puede ser ejecutado por un Socket.',
-admin: '✐ Este comando solo puede ser ejecutado por los Administradores del Grupo.',
-botAdmin: '✐ Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
+socket: '🐉🌀 Este comando solo puede ser ejecutado por un Socket Gotenks.',
+admin: '⚡ Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+botAdmin: '🌀 Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo Gotenks.'
 }
 
 global.APIs = {
